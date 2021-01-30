e25558bb8e9bcfd30195dbc3cfc5a478a30e27d8
Mehran Jalili <mehranj73@gmail.com>
2021-01-30 21:56:52 +0330

    Part 3.

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ dotenv.config({path: './config/config.env'})
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'});
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
-    console.log(`Server runnning in ${process.env.NODE_ENV} mode on porn ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on porn ${PORT}`)
 );
