6c2b29f52a7338f760978ec96fc7ec1a7bfdde52
Mehran Jalili <mehranj73@gmail.com>
2021-02-03 21:46:10 +0330

    Add mongodb.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 
-const bootcamps = require('./routes/bootcamps')
 
 dotenv.config({path: './config/config.env'})
+
+connectDB();
+
+const bootcamps = require('./routes/bootcamps')
 
 const app = express();
 
@@ -14,12 +18,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => promise.exit(1))
+});
